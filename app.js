feeff61579779b8e2244145c4ce09a84606b32db
Simran Phade <SP0C95628@techmahindra.com>
2023-09-14 12:55:59 +0530

    updated app.js file msg

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Testing take1!'));
+app.get('/', (req, res) => res.send('Testing take1 successful!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
